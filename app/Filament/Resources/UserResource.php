9f716417c7e798cf9c29e7c9fd8a6c1fffd9bc8f
aabid <115284244+aabidk20@users.noreply.github.com>
2025-09-09 00:22:25 +0530

    User icon

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,32 +3,26 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
- 
-    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
 
     protected static ?string $navigationLabel = 'User';
 
     protected static ?string $modelLabel = 'Users';
 
-
     protected static ?string $slug = 'users';
 
     protected static ?string $navigationGroup = 'User Management';
-
 
     public static function form(Form $form): Form
     {
